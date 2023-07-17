6259331e59c4f10d8110a888f8db4fc5cb6325f5
Gabriel Gerez <gabriel.a.gerez.s@uit.no>
2023-07-17 21:16:30 +0200

    fix Permittivity

--- MRPyCM/soluteFunctions/Permittivity.py
+++ MRPyCM/soluteFunctions/Permittivity.py
@@ -6,7 +6,7 @@ class Linear(ShiftFunction):
     
     def __call__(self, r):
         C_eval = self.C(r)
-        permittivity = C_eval*(self.inside - self.outside) + self.outside
+        permittivity = self.inside*C_eval + self.outside*(1.0 - C_eval)
         return permittivity
 
 
@@ -15,6 +15,6 @@ class Exponential(ShiftFunction):
     
     def __call__(self, r):
         C_eval = self.C(r)
-        permittivity = self.inside*np.exp((np.log((self.inside/self.outside)))*(1.0 - C_eval))
+        permittivity = self.inside*np.exp((np.log((self.outside/self.inside)))*(1.0 - C_eval))
     
         return permittivity
