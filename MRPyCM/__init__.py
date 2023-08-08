24819bde37b12d2fc29d2d608715b6af4c7ca1f1
Gabriel Gerez <gabriel.a.gerez.s@uit.no>
2023-08-08 11:11:21 +0200

    add import to KAIN

--- MRPyCM/__init__.py
+++ MRPyCM/__init__.py
@@ -1,5 +1,10 @@
+import sys 
 from .soluteFunctions import *
 from .solventSolvers import *
 from .utilities import *
+
+sys.path.insert(1, 'external/response/src/')
+
+import KAIN
 
 __all__ = ["soluteFunctions", "solventSolvers", "utilities"]
